29a3ad72c7e5689d0d827e437f55b3ee2a96055c
cagoncil <crystallophobia@gmail.com>
2022-06-12 16:18:00 -0700

    add new and modify existing components and functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,11 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Button = ({ color, text }) => {
-    const onClick = () => {
-        console.log('click');
-    }
+const Button = ({ color, text, onClick }) => {
 
     return (
-        <button onClick={onClick} style={{ backgroundColor: color }} className='btn'>
+        <button onClick={onClick} 
+        style={{ backgroundColor: color }} 
+        className='btn'>
             {text}
         </button>
     )
@@ -19,6 +18,7 @@ Button.defaultProps = {
 Button.propTypes = {
     text: PropTypes.string,
     color: PropTypes.string,
+    onClick: PropTypes.func
 }
 
 export default Button
