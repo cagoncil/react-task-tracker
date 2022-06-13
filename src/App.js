d7d1de437201c282f9c157c994ee3395aac7d1b3
cagoncil <crystallophobia@gmail.com>
2022-06-13 10:15:10 -0700

    modify css and src files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,6 +26,13 @@ function App() {
     }
   ]);
 
+  // Add Task
+  const addTask = task => {
+    const id = Math.floor(Math.random() * 99999) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  }
+
   // Delete Task
   const deleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id))
@@ -41,6 +50,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? 
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 
         'No tasks left!'
